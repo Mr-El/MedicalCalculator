dfad5cdfbdbcd057ee8a010e468774aaa8fa59b1
Mr. El <elanebritton96@gmail.com>
2017-01-17 09:39:17 -0500

    Added OnClickListener

--- app/src/main/java/net/androidbootcamp/medicalcalculator/MainActivity.java
+++ app/src/main/java/net/androidbootcamp/medicalcalculator/MainActivity.java
@@ -3,8 +3,11 @@ package net.androidbootcamp.medicalcalculator;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
 
 //Elliott Britton 1/17/2017
 public class MainActivity extends AppCompatActivity {
@@ -24,5 +27,14 @@ public class MainActivity extends AppCompatActivity {
         final EditText weight = (EditText) findViewById(R.id.txtWeight);
         final RadioButton lbToKilo = (RadioButton)findViewById(R.id.radLbToKilo);
         final RadioButton kiloToLb = (RadioButton)findViewById(R.id.radKiloToLb);
+        final TextView result = (TextView)findViewById(R.id.txtResult);
+        Button convert = (Button)findViewById(R.id.btnConvert);
+
+        convert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }

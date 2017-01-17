5d210f25c0de0dfd7190261a37aa9f43ae51ab49
Mr. El <elanebritton96@gmail.com>
2017-01-17 09:35:29 -0500

    added in radiobutton class

--- app/src/main/java/net/androidbootcamp/medicalcalculator/MainActivity.java
+++ app/src/main/java/net/androidbootcamp/medicalcalculator/MainActivity.java
@@ -1,8 +1,12 @@
 package net.androidbootcamp.medicalcalculator;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
+//Elliott Britton 1/17/2017
 public class MainActivity extends AppCompatActivity {
     double conversionRate = 2.2;
     double weightEntered;
@@ -16,5 +20,9 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setLogo(R.mipmap.ic_launcher);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        final EditText weight = (EditText) findViewById(R.id.txtWeight);
+        final RadioButton lbToKilo = (RadioButton)findViewById(R.id.radLbToKilo);
+        final RadioButton kiloToLb = (RadioButton)findViewById(R.id.radKiloToLb);
     }
 }
